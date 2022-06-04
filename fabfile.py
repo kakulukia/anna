7935a77b062a26cec2ffe7218bb744b5710a9798
Andy <andy@freilandkiwis.de>
2022-06-04 11:51:14 +0200

    fabric ist nun mit dabei

---
+++ fabfile.py
@@ -0,0 +1,92 @@
+from fabric.colors import green
+from fabric.context_managers import cd
+from fabric.operations import local, run
+from fabric.state import env
+
+env.shell = "/bin/zsh -c"
+env.output_prefix = False
+APP_NAME = "anna"
+
+env.path = f"/opt/www/{APP_NAME}"
+env.hosts = ["anna.liebendgern.de"]
+
+
+# # DEPLOYMENT TARGETS
+# # ####################
+# def stage():
+#     env.environment = "stage"
+#     env.hosts = ["servername"]
+
+
+# def live():
+#     env.environment = "live"
+#     env.hosts = ["servername"]
+
+
+# T A S K S
+# ###########
+def deploy_only():
+    """Pull all updates from the remote repository."""
+    with cd(env.path):
+        print(green("updating from repository .."))
+        run("git pull")
+
+
+def clear_cache():
+    with cd(env.path):
+        print(green("\ndeleting cache .."))
+        manage("clear_cache")
+        # manage("thumbnail clear_delete_all")
+
+
+def restart():
+    """Restart nginx and the backend worker."""
+    print(green("restarting server .."))
+    run(f"pm2 restart {APP_NAME}")
+
+    clear_cache()
+
+
+def deploy():
+    deploy_only()
+    update_static()
+
+    restart()
+
+
+def migrate():
+    """
+    Pull all updates from the remote repository.
+    Migrates the database and installs new lib versions from requirements.
+    Static files are also collected.
+    """
+    deploy_only()
+
+    with cd(env.path):
+        print(green("updating packages .."))
+        # this might otherwise cause some trouble: https://github.com/python-poetry/poetry/issues/732
+        # up until poetry 1.2
+        run("poetry run pip install --upgrade pip setuptools")
+        run("poetry install")
+
+        print(green("migrating database .."))
+        manage("migrate --noinput")
+
+        update_static()
+
+    restart()
+
+
+def update_static():
+    with cd(env.path):
+
+        print(green("compressing files .."))
+        manage("compress -e pug,html --force")
+        manage("compilescss")
+
+        print(green("collecting static files .."))
+        manage("collectstatic --noinput")
+
+
+def manage(command):
+    run("poetry run ./manage.py " + command)

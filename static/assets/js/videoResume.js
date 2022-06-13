1c5c8d73cbc1dd3d37604e26efc32fee780b6bf1
Andy <andy@freilandkiwis.de>
2022-06-13 21:15:40 +0200

    Jetzt wahrscheinlich wirklich repariert .. Safari ist echt pingelig!

--- static/assets/js/videoResume.js
+++ static/assets/js/videoResume.js
@@ -4,11 +4,10 @@ videoName = videoName.replace(/\.mp(3|4).*/, "");
 videoName = videoName.replace(/.*\//, "");
 
 (() => {
-  getCurrentTime = localStorage.getItem(`${videoName}ResumeTime`);
-  sampleVideo.currentTime = getCurrentTime - 1;
+  const getCurrentTime = localStorage.getItem(`${videoName}ResumeTime`) ;
+  sampleVideo.currentTime = getCurrentTime;
 })();
 
-// WHEN THE USER SWITCHES THE WINDOW OR IT GETS DESTROYED THEN THE CURRENT PLAYBACK TIME OF THE VIDEO IS SAVED IN LOCAL STORAGE
 document.addEventListener("visibilitychange", () => {
   if (document.hidden) {
     localStorage.setItem(`${videoName}ResumeTime`, sampleVideo.currentTime);

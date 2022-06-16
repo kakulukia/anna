d6dbfd64cbb8a420248a1917b9915a995478eae7
Andy <andy@freilandkiwis.de>
2022-06-16 20:45:05 +0200

    LIEBENDGERN-12 Frontend: Home Info

--- static/assets/js/videoResume.js
+++ static/assets/js/videoResume.js
@@ -1,18 +1,21 @@
 let sampleVideo = document.getElementById("videoDings");
-let videoName = sampleVideo.childNodes[1].src;
-videoName = videoName.replace(/\.mp(3|4).*/, "");
-videoName = videoName.replace(/.*\//, "");
 
-(() => {
-  const getCurrentTime = localStorage.getItem(`${videoName}ResumeTime`) ;
-  sampleVideo.currentTime = getCurrentTime;
-})();
+if (sampleVideo) {
+  let videoName = sampleVideo.childNodes[1].src;
+  videoName = videoName.replace(/\.mp(3|4).*/, "");
+  videoName = videoName.replace(/.*\//, "");
 
-document.addEventListener("visibilitychange", () => {
-  if (document.hidden) {
-    localStorage.setItem(`${videoName}ResumeTime`, sampleVideo.currentTime);
-    return;
-  } else {
-    return;
-  }
-});
+  (() => {
+    const getCurrentTime = localStorage.getItem(`${videoName}ResumeTime`) ;
+    sampleVideo.currentTime = getCurrentTime;
+  })();
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+      localStorage.setItem(`${videoName}ResumeTime`, sampleVideo.currentTime);
+      return;
+    } else {
+      return;
+    }
+  });
+}

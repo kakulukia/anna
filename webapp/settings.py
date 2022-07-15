ede50fda4eaf2f6f5779c074acccc4ca40b8948c
Andy <andy@freilandkiwis.de>
2022-07-15 21:28:17 +0200

    kein Debug mehr

--- webapp/settings.py
+++ webapp/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = secrets.SECRET_KEY
 SITE_ID = 1
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = [

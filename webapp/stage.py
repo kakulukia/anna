7aff6bef0ef42a57ef722690d4624f1c24e862a1
Andy <andy@freilandkiwis.de>
2023-01-11 21:15:51 +0100

    nu aber wirklich

--- webapp/stage.py
+++ webapp/stage.py
@@ -2,8 +2,8 @@ from .settings import *
 
 STAGE = True
 
-JAZZMIN_SETTINGS["site_logo"] = "img/mylogo-test-login.png" if STAGE else "img/mylogo-gruen-login.png",
-JAZZMIN_SETTINGS["site_icon"] = "assets/img/favicon-test.png" if STAGE else "assets/img/favicon.png",
+JAZZMIN_SETTINGS["site_logo"] = "img/mylogo-test-login.png" if STAGE else "img/mylogo-gruen-login.png"
+JAZZMIN_SETTINGS["site_icon"] = "assets/img/favicon-test.png" if STAGE else "assets/img/favicon.png"
 JAZZMIN_SETTINGS["topmenu_links"] = [
     {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
     {"name": f"Seite Anzeigen ({'STAGE' if STAGE else 'LIVE'})", "url": "index", "permissions": []},

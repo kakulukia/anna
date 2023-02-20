0915a489fe05f83643cb3a1bce6d2f849c107574
Andy <andy@freilandkiwis.de>
2023-02-20 14:07:22 +0100

    #LIEBENDGERN-122 qa monique

--- application/admin.py
+++ application/admin.py
@@ -53,7 +53,7 @@ class ModuleAdmin(admin.ModelAdmin):
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ['name', 'ordering', 'length', 'next']
+    list_display = ['name', 'ordering', 'length', 'next', 'attachment']
     list_filter = ['module']
     search_fields = ['name']
 

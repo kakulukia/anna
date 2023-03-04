3ffbfa264f5fee8d64251cb365450eda55ac6073
Andy <andy@freilandkiwis.de>
2023-03-04 10:43:08 +0100

    das sollte die richtigen settings laden

--- webapp/test-anna.sh
+++ webapp/test-anna.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-DJANGO_SETTINGS_MODULE=webapp.stage
-.venv/bin/gunicorn --worker-class='gevent' --bind unix:/tmp/test-anna.sock webapp.wsgi:application
+DJANGO_SETTINGS_MODULE=webapp.stage .venv/bin/gunicorn --worker-class='gevent' --bind unix:/tmp/test-anna.sock webapp.wsgi:application

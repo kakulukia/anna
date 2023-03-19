c7b88533047b55e6a9e94df9b42dd823ac0f01ef
Andy <andy@freilandkiwis.de>
2023-03-19 21:06:14 +0100

    Das sollte besser funktionieren.

--- webapp/test-anna.sh
+++ webapp/test-anna.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-export DJANGO_SETTINGS_MODULE=settings.stage && .venv/bin/gunicorn --worker-class='gevent' --bind unix:/tmp/test-anna.sock webapp.wsgi:application
+.venv/bin/gunicorn --env DJANGO_SETTINGS_MODULE=settings.stage --worker-class='gevent' --bind unix:/tmp/test-anna.sock webapp.wsgi:application

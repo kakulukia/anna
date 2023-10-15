61a5ef17822011b920e422bcb90bdee3da42afd5
Andy <andy@freilandkiwis.de>
2023-10-15 14:08:41 +0200

    Wir wollen doch eigentlich nur das Datum vergleichen 🙈

--- application/signals.py
+++ application/signals.py
@@ -3,6 +3,6 @@ from webapp.tasks import update_close
 
 
 def update_last_login(sender, instance: User, **kwargs):
-    if instance.last_login != User.data.get(id=instance.id).last_login:
+    if instance.last_login.date() != User.data.get(id=instance.id).last_login.date():
         data = {"custom.cf_PnRnq2gTqYTGElh7iX0MIQshSQtXUmqPDLjYy5Dtqt2": instance.last_login.date().isoformat()}
         update_close(instance.lead_id, data)

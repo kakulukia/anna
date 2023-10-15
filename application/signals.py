2a5392825b802bd6a7cec214f3cce218cbfe0cb3
Andy <andy@freilandkiwis.de>
2023-10-15 14:01:29 +0200

    da war was faul

--- application/signals.py
+++ application/signals.py
@@ -3,6 +3,5 @@ from webapp.tasks import update_close
 
 
 def update_last_login(sender, instance: User, **kwargs):
-    if instance.last_login != User.data.get(id=instance.id).last.login:
-        data = {"custom.cf_PnRnq2gTqYTGElh7iX0MIQshSQtXUmqPDLjYy5Dtqt2": instance.last_login.date().isoformat()}
-        update_close(instance.lead_id, data)
+    data = {"custom.cf_PnRnq2gTqYTGElh7iX0MIQshSQtXUmqPDLjYy5Dtqt2": instance.last_login.date().isoformat()}
+    update_close(instance.lead_id, data)

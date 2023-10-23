1c2a84d0f203eec3fd8ac898c8f40021b9972501
Andy <andy@freilandkiwis.de>
2023-10-23 22:52:36 +0200

    fix für user, die sich noch nie eingeloggt haben

--- application/signals.py
+++ application/signals.py
@@ -3,8 +3,11 @@ from webapp.tasks import update_close
 
 
 def update_last_login(sender, instance: User, **kwargs):
-    if not instance.lead_id:
+    if not instance.lead_id or not instance.last_login:
         return
-    if instance.last_login.date() != User.data.get(id=instance.id).last_login.date():
+
+    old_user = User.data.get(id=instance.id)
+
+    if not old_user.last_login or instance.last_login.date() != old_user.last_login.date():
         data = {"custom.cf_PnRnq2gTqYTGElh7iX0MIQshSQtXUmqPDLjYy5Dtqt2": instance.last_login.date().isoformat()}
         update_close(instance.lead_id, data)
